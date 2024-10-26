4c5f3c358f2f060a5d8a72032d63c325d8435a75
shupira <shupira@jyubira-ubuntu.myguest.virtualbox.org>
2024-10-26 21:37:05 +0900

    スペルミスを修正

--- src/global_var.py
+++ src/global_var.py
@@ -6,4 +6,4 @@ from pathlib import Path
 
 deadline = datetime.strptime('2024-09-30 23:59:59', '%Y-%m-%d %H:%M:%S')
 
-runner_path = Path('../PyNose-AES2021/runner.py').resolve(strict=True)
+runner_path = Path('./PyNose-ASE2021/runner.py').resolve(strict=True)
