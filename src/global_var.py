7220be1e1294db65176530b13c917e8af2280260
fushi <k807026c@mails.cc.ehime-u.ac.jp>
2024-10-26 20:27:22 +0900

    グローバル変数を記したファイルを新規実装．

---
+++ src/global_var.py
@@ -0,0 +1,9 @@
+"""
+統一して使用するデータ．
+"""
+from datetime import datetime
+from pathlib import Path
+
+deadline = datetime.strptime('2024-09-30 23:59:59', '%Y-%m-%d %H:%M:%S')
+
+runner_path = Path('../PyNose-AES2021/runner.py').resolve(strict=True)
