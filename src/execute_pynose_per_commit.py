d445a92da9685d6aa32149420da744169ac4e914
fushi <k807026c@mails.cc.ehime-u.ac.jp>
2024-10-28 07:18:02 +0900

    Ctrl+c 時にプログラムが終了するように変更．

--- src/execute_pynose_per_commit.py
+++ src/execute_pynose_per_commit.py
@@ -67,6 +67,7 @@ def main():
                 except FileNotFoundError:
                     time.sleep(1)
                     shutil.rmtree(pynose_instance_path)
+                break
             else:
                 print(f'{repo_name} {index}/{len(commit_hashes)}')
 
