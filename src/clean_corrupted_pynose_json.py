926da061d6c545c645b0899800bed9e6dbb91c4a
fushi <k807026c@mails.cc.ehime-u.ac.jp>
2024-11-22 17:12:38 +0900

    PyNose が出力した破損している json ファイルを削除するプログラムを実装．

---
+++ src/clean_corrupted_pynose_json.py
@@ -0,0 +1,20 @@
+"""
+Ctrl + c などによってたまに PyNose の出力する json ファイルが破損することがある．
+それらを検出し，削除するプログラム．
+具体的には，ファイルの読み込みに失敗するか，解析に失敗した場合に削除する．
+"""
+from pathlib import Path
+
+from pynose_result_manager import PyNoseResultManager
+
+
+result_root = Path('../result/execute_pynose_per_commit').resolve(strict=True)
+pynose_result_paths = result_root.rglob('*.json')
+
+for pynose_result_path in pynose_result_paths:
+    try:
+        pynose_result_manager = PyNoseResultManager(pynose_result_path)
+        pynose_result_manager.count_test_smells_per_file()
+    except: # noqa
+        print(pynose_result_path.as_posix())
+        pynose_result_path.unlink()
