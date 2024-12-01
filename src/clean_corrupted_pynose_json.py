2ebb77fe0dca2f27b678f1760780b737b4b5672f
fushi <k807026c@mails.cc.ehime-u.ac.jp>
2024-12-02 04:51:18 +0900

    ファイル数が多すぎることによる大幅なオーバーヘッドの増加を確認したため，逐次処理に再度変更．

--- src/clean_corrupted_pynose_json.py
+++ src/clean_corrupted_pynose_json.py
@@ -4,7 +4,6 @@ Ctrl + c などによってたまに PyNose の出力する json ファイルが
 具体的には，ファイルの読み込みに失敗するか，解析に失敗した場合に削除する．
 """
 import sys
-from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 from tqdm import tqdm
@@ -20,27 +19,14 @@ def main():
     file_paths = result_root.rglob('*.json')
     file_cnt = sum(1 for _ in result_root.rglob('*.json'))
 
-    with ThreadPoolExecutor() as executor:
-        with tqdm(total=file_cnt) as pbar:
-            futures = [executor.submit(clean_corrupted_file, file_path)
-                       for file_path in file_paths]
-            for future in futures:
-                future.result()
-                pbar.update()
-
-
-def clean_corrupted_file(file_path: Path):
-    """
-    PyNoseResultManager でファイルの破損を確認し，
-    破損していれば削除する．
-    """
-    try:
-        pynose_result_manager = PyNoseResultManager(file_path)
-        pynose_result_manager.count_test_smells_per_file()
-    except KeyboardInterrupt:
-        sys.exit(0)
-    except: # noqa
-        file_path.unlink()
+    for file_path in tqdm(file_paths, total=file_cnt):
+        try:
+            pynose_result_manager = PyNoseResultManager(file_path)
+            pynose_result_manager.count_test_smells_per_file()
+        except KeyboardInterrupt:
+            sys.exit(0)
+        except: # noqa
+            file_path.unlink()
 
 
 if __name__ == '__main__':
--- src/compress_pynose_result.py
+++ src/compress_pynose_result.py
@@ -2,7 +2,6 @@
 PyNose の結果は約 2 TB存在するのでそれらを圧縮するプログラム．
 """
 import json
-from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 from tqdm import tqdm
@@ -21,30 +20,19 @@ def main():
     this_file_name = Path(__file__).stem
     output_root = Path(f'../result/{this_file_name}').resolve()
     output_root.mkdir(exist_ok=True)
+
+    for result_dir in input_root.iterdir():
+        output_root.joinpath(result_dir.name).mkdir(exist_ok=True)
+
     output_files = [output_root.joinpath(*file_path.parts[-2:])
                     for file_path in input_files]
 
-    with ThreadPoolExecutor() as executor:
-        with tqdm(total=len(input_files)) as pbar:
-            futures = [executor.submit(compress_file, input_file, output_file)
-                       for input_file, output_file
-                       in zip(input_files, output_files)]
-            for future in futures:
-                future.result()
-                pbar.update()
+    for input_file, output_file in zip(tqdm(input_files), output_files):
+        manager = PyNoseResultManager(input_file)
+        compressed_data = manager.count_test_smells_per_file()
 
-
-def compress_file(src_file: Path, dst_file: Path):
-    """
-    ファイル名と各テストスメルの出現数のみを保持し，
-    それ以外は除去する．
-    """
-    manager = PyNoseResultManager(src_file)
-    compressed_data = manager.count_test_smells_per_file()
-
-    dst_file.parent.mkdir(exist_ok=True)
-    with dst_file.open('w') as f:
-        json.dump(compressed_data, f)
+        with output_file.open('w') as f:
+            json.dump(compressed_data, f)
 
 
 if __name__ == '__main__':
