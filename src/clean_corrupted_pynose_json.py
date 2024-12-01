ddc439223ba1cd33b24d09909d259c3a793c146f
fushi <k807026c@mails.cc.ehime-u.ac.jp>
2024-12-02 02:17:57 +0900

    並行処理で高速化．

--- src/clean_corrupted_pynose_json.py
+++ src/clean_corrupted_pynose_json.py
@@ -3,6 +3,8 @@ Ctrl + c などによってたまに PyNose の出力する json ファイルが
 それらを検出し，削除するプログラム．
 具体的には，ファイルの読み込みに失敗するか，解析に失敗した場合に削除する．
 """
+import sys
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 from tqdm import tqdm
@@ -10,15 +12,32 @@ from tqdm import tqdm
 from pynose_result_manager import PyNoseResultManager
 
 
-result_root = Path('../result/execute_pynose_per_commit').resolve(strict=True)
-pynose_result_paths = result_root.rglob('*.json')
+def main():
+    """
+    読み込みと加工ができないファイルを削除する．
+    """
+    result_root = Path('../result/execute_pynose_per_commit').resolve()
+    pynose_result_paths = [file_path for result_dir in result_root.iterdir()
+                           for file_path in result_dir.glob('*.json')]
 
-for pynose_result_path in tqdm(pynose_result_paths):
+    with ThreadPoolExecutor() as executor:
+        list(tqdm(executor.map(clean_corrupted_file, pynose_result_paths),
+                  total=len(pynose_result_paths)))
+
+
+def clean_corrupted_file(file_path: Path):
+    """
+    PyNoseResultManager でファイルの破損を確認し，
+    破損していれば削除する．
+    """
     try:
-        pynose_result_manager = PyNoseResultManager(pynose_result_path)
+        pynose_result_manager = PyNoseResultManager(file_path)
         pynose_result_manager.count_test_smells_per_file()
     except KeyboardInterrupt:
-        break
+        sys.exit(0)
     except: # noqa
-        print(pynose_result_path.as_posix())
-        pynose_result_path.unlink()
+        file_path.unlink()
+
+
+if __name__ == '__main__':
+    main()
--- src/compress_pynose_result.py
+++ src/compress_pynose_result.py
@@ -2,28 +2,46 @@
 PyNose の結果は約 2 TB存在するのでそれらを圧縮するプログラム．
 """
 import json
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
+
 from tqdm import tqdm
 
 from pynose_result_manager import PyNoseResultManager
 
 
-this_file_name = Path(__file__).stem
-output_root = Path(f'../result/{this_file_name}')
-output_root.mkdir(exist_ok=True)
+def main():
+    """
+    不必要な情報を省き，見やすさを排除することで圧縮を図る．
+    """
+    input_root = Path('../result/execute_pynose_per_commit').resolve()
+    input_files = [file_path for result_dir in input_root.iterdir()
+                   for file_path in result_dir.glob('*.json')]
+
+    this_file_name = Path(__file__).stem
+    output_root = Path(f'../result/{this_file_name}').resolve()
+    output_root.mkdir(exist_ok=True)
+    output_files = [output_root.joinpath(*file_path.parts[-2:])
+                    for file_path in input_files]
+
+    with ThreadPoolExecutor() as executor:
+        list(tqdm(executor.map(lambda files: compress_file(*files),
+                               zip(input_files, output_files)),
+                  total=len(input_files)))
 
 
-input_root = Path('../result/execute_pynose_per_commit').resolve()
+def compress_file(src_file: Path, dst_file: Path):
+    """
+    ファイル名と各テストスメルの出現数のみを保持し，
+    それ以外は除去する．
+    """
+    manager = PyNoseResultManager(src_file)
+    compressed_data = manager.count_test_smells_per_file()
 
-# リポジトリ単位のループ
-for repo_dir in tqdm(input_root.glob('*')):
-    output_repo_dir = output_root.joinpath(repo_dir.name)
-    output_repo_dir.mkdir(exist_ok=True)
+    dst_file.parent.mkdir(exist_ok=True)
+    with dst_file.open('w') as f:
+        json.dump(compressed_data, f)
 
-    # 各JSONファイルのループ
-    for json_file_path in tqdm(repo_dir.glob('*.json'), leave=False):
-        output_file_name = json_file_path.name
-        pynose_result_manager = PyNoseResultManager(json_file_path)
-        json_data = pynose_result_manager.count_test_smells_per_file()
-        with output_repo_dir.joinpath(output_file_name).open('w') as f:
-            json.dump(json_data, f)
+
+if __name__ == '__main__':
+    main()
