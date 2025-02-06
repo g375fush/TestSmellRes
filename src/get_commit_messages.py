e5a40be1a4f9367289161069c18acd576558b9fd
fushi <k807026c@mails.cc.ehime-u.ac.jp>
2025-02-06 16:08:50 +0900

    辞書型にデータ型を変更．ファイル名を変更．

--- src/get_commit_messages.py
+++ src/get_commit_messages.py
@@ -18,10 +18,17 @@ for repo_prefix in tqdm(target_list):
     repo_path = repo_prefix / repo_name
     repo = Repo(repo_path)
 
+    commit_hashes = repo.get_commit_hashes(until=deadline)
     commit_messages = repo.get_commit_messages(until=deadline)
+
+    commit_dict = {commit_hash: commit_message
+                   for commit_hash, commit_message in
+                   zip(commit_hashes, commit_messages)}
+
     result_dir = Path(f'../result/{this_file_name}/{repo_name}')
     result_dir.mkdir(exist_ok=True, parents=True)
     result_file_path = result_dir / f'{repo_name}.json'
+
     with result_file_path.open('w') as f:
-        json.dump(commit_messages, f, indent=4)
+        json.dump(commit_dict, f, indent=4)
         f.write('\n')
